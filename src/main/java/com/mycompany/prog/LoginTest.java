50952d7ccd6d70c10092ead8680845b30049ce03
Nokukhanya <Nokukhanya@LAPTOP-11GN7IU1>
2026-09-18 07:56:03 +0200

    PROG

---
+++ src/main/java/com/mycompany/prog/LoginTest.java
@@ -0,0 +1,60 @@
+package com.mycompany.prog;
+
+public class LoginTest.java{
+
+private String storedUsername;
+private String storedPassword;
+private String storedCellNumber;
+private String firstName;
+private String lastName;
+
+public boolean checkUserName(String username) {
+if (username == null) return false;
+return username.contains("_") && username.length() <= 5;
+}
+
+public boolean checkPasswordComplexity(String password) {
+if (password == null) return false;
+String regex = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=!]).{8,}$";
+return password.matches(regex);
+}
+
+public boolean checkCellPhoneNumber(String cellNumber) {
+if (cellNumber == null) return false;
+String regex = "^\\+27[0-9]{9}$";
+return cellNumber.matches(regex);
+}
+
+public String registerUser(String username, String password, String cellNumber, String fName, String lName) {
+if (!checkUserName(username)) {
+return "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.";
+}
+if (!checkPasswordComplexity(password)) {
+return "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
+}
+if (!checkCellPhoneNumber(cellNumber)) {
+return "Cell phone number incorrectly formatted or does not contain international code.";
+}
+
+this.storedUsername = username;
+this.storedPassword = password;
+this.storedCellNumber = cellNumber;
+this.firstName = fName;
+this.lastName = lName;
+
+return "Username successfully captured.\nPassword successfully captured.\nCell phone number successfully added.";
+}
+
+public boolean loginUser(String username, String password) {
+if (this.storedUsername == null || this.storedPassword == null) return false;
+return username.equals(this.storedUsername) && password.equals(this.storedPassword);
+}
+
+public String returnLoginStatus(boolean isLoggedIn) {
+if (isLoggedIn) {
+return "Welcome " + firstName + ", " + lastName + " it is great to see you again.";
+} else {
+return "Username or password incorrect, please try again.";
+}
+}
+}
